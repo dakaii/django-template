b2ec0ea4a02493a25b601e31a2289b1ef931e7dc
Daiki Nakashita <daiki815@gmail.com>
2020-04-09 20:28:45 +0900

    use djangorestframework-simplejwt for authentication

--- core/settings.py
+++ core/settings.py
@@ -75,7 +75,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'users.authentication.JSONWebTokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
